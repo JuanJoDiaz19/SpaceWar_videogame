4e13c20d33ff35b3a9142684c20c45762f63d175
Annsof <Annsof>
2022-11-28 16:41:28 -0500

    Adding home screen

--- src/main/java/com/example/tank_battle/GameMain.java
+++ src/main/java/com/example/tank_battle/GameMain.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class GameMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GameMain.class.getResource("gameCanvas.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GameMain.class.getResource("homeMenu.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
---
+++ src/main/java/com/example/tank_battle/HomeMenuController.java
@@ -0,0 +1,27 @@
+package com.example.tank_battle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class HomeMenuController {
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private ImageView scoresButton;
+
+    @FXML
+    void play(ActionEvent event) {
+
+    }
+
+    @FXML
+    void scores(MouseEvent event) {
+
+    }
+
+}

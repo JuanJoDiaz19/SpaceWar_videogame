867e2714ec7a4d101bf1d870883a1309fa82023e
Santyet <santyet7@hotmail.com>
2022-11-30 11:28:43 -0500

    esta va pa claudia <3

--- src/main/java/com/example/tank_battle/NamesController.java
+++ src/main/java/com/example/tank_battle/NamesController.java
@@ -3,6 +3,7 @@ package com.example.tank_battle;
 import com.example.tank_battle.control.Singleton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -10,14 +11,22 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.InputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class NamesController {
+public class NamesController implements Initializable {
 
     @FXML
     private Button cancelBTN;
 
     @FXML
     private Label label1;
+
+    @FXML
+    private Label label2;
+
+    @FXML
+    private Label label3;
 
     @FXML
     private TextField p1Name;
@@ -34,7 +43,7 @@ public class NamesController {
     @FXML
     void cancelA(ActionEvent event) {
         Stage currentStage = (Stage) cancelBTN.getScene().getWindow();
-        GameMain.hideWindow("winning.fxml");
+        GameMain.hideWindow("addNames.fxml");
         currentStage.hide();
     }
 
@@ -46,5 +55,22 @@ public class NamesController {
         currentStage.hide();
         GameMain.hideWindow("homeMenu.fxml");
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setFonts();
+    }
+
+    InputStream is1 = getClass().getResourceAsStream("/fonts/Pixeboy.ttf");
+    private Font pixeman1 = Font.loadFont(is1, 20);
+
+    private void setFonts(){
+        p1Name.setFont(pixeman1);
+        p2Name.setFont(pixeman1);
+        p3Name.setFont(pixeman1);
+        label1.setFont(pixeman1);
+        label2.setFont(pixeman1);
+        label3.setFont(pixeman1);
     }
 }
